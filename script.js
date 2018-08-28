a7b82f055340f246d5dda280891b325b64327ee9
Clinton Johnson <dragoon1246224@gmail.com>
2018-08-28 10:00:09 -0400

    Done

---
+++ script.js
@@ -0,0 +1,22 @@
+
+let boxTop = 200;
+let boxLeft = 200;
+'use strict';
+
+document.addEventListener('keydown', (event) => {
+  const keyName = event.key;
+  console.log('keydown event\n\n' + 'key: ' + keyName);
+
+  if (keyName === "ArrowUp") 
+  {boxTop -= 10}
+  else if (keyName === "ArrowDown") 
+  {boxTop += 10}
+  else if (keyName === "ArrowLeft")
+  {boxLeft -= 10}
+  else if (keyName === "ArrowRight")
+  {boxLeft += 10}
+
+  document.getElementById("box").style.top = boxTop + "px";
+  document.getElementById("box").style.left = boxLeft + "px";
+
+});
